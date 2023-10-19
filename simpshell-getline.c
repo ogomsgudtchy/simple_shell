af0811fae7e28b71a18f76949f899cc0dfb53aae
CambridgeMM <cambi.matome@gmail.com>
2023-10-19 01:34:57 -0700

    simpshell

--- simpshell-getline.c
+++ simpshell-getline.c
@@ -1,9 +1,5 @@
 #include "shell.h"
 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-
 /**
  * _realloc - Function that reallocates memory block using malloc and free.
  * @ptr: Pointer to the memory previously allocated.
@@ -131,24 +127,14 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 
 		if (input >= 120)
 			buffer = _realloc(buffer, input, input + 1);
-buffer[input] = c;
+
+		buffer[input] = c;
 		input++;
 	}
-<<<<<<< HEAD
-buffer[input] = '\0';
+	buffer[input] = '\0';
 assign_lineptr(lineptr, n, buffer, input);
 ret = input;
-if (r != 0)
-input = 0;
-return (ret);
-=======
-	buffer[input] = '\0';
-
-	assign_lineptr(lineptr, n, buffer, input);
-
-	ret = input;
 	if (r != 0)
 		input = 0;
 	return (ret);
->>>>>>> 083766a30657ab834e556ec32df50e21ca197159
 }
